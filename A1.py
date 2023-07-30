00ce57b83f69fcfebb09950e3fec0fe6067ac621
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-07-31 02:03:40 +0530

    Create A1.py

---
+++ A1.py
@@ -0,0 +1,10 @@
+#write a python program to perform all arithmetic operation by assigning two variables.
+
+a=40
+b=30
+print("Arithmetic Operations --- ")
+print("Sum : ", a+b)
+print("Difference : ", a-b)
+print("Product : ", a*b)
+print("Division of 1st by 2nd : ", a/b)
+print("Remainder when 1st divided by 2nd : ", a%b)
