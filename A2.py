94726f424bc39419c3ebe074d176d28ddb1d261a
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-07-31 02:04:11 +0530

    Create A2.py

---
+++ A2.py
@@ -0,0 +1,12 @@
+#write a python program to assign two variables and swap them without using 3rd variable.
+
+a=10
+b=20
+print("Numbers before Swapping : ")
+print("a = ", a)
+print("b = ", b)
+print()
+a,b=b,a
+print("Numbers after Swapping : ")
+print("a = ", a)
+print("b = ", b)
