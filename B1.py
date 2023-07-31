239bb22cd460c943599a3c7641b01e17338087ec
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-07-31 07:33:59 +0530

    Create B1.py

---
+++ B1.py
@@ -0,0 +1,22 @@
+#A school has following rules for grading system : 
+# a. below 25 - F
+# b. 26 to 45 - E
+# c. 46 to 50 - D
+# d. 51 to 60 - C
+# e. 61 to 80 - B
+# f. Above 80 - A
+#Ask user to enter marks and print the corresponding grade.
+
+marks = int (input ("Enter Marks : "))
+if marks <= 25:
+  print("Grade : F")
+elif marks >= 26 and marks <= 45:
+  print("Grade : E")
+elif marks >= 46 and marks <= 50:
+  print("Grade : D")
+elif marks >= 51 and marks <= 60:
+  print("Grade : C")
+elif marks >= 61 and marks <= 80:
+  print("Grade : B")
+else :
+  print("Grade : A")
