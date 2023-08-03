ef412f4819a8275e87c529f32142768dfe1d3b24
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-03 11:53:38 +0530

    Update divisible.py

--- divisible.py
+++ divisible.py
@@ -7,4 +7,9 @@
 a = int (input("Enter a number : "))
 if a%3 == 0 and a%5 == 0:
   print("Divisible by both 3 as well as 5.")
-elif 
+elif a%3 == 0:
+  print("Divisible by 3 and not by 5.")
+elif a%5 == 0:
+  print("Divisible by 5 and not by 3.")
+else :
+  print("Not divisible by 3 as well as 5.")
