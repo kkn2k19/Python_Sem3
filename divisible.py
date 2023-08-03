69920a6a9bebe578bd256aa786b6ee0284d09ac4
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-03 11:51:46 +0530

    Create divisible.py

---
+++ divisible.py
@@ -0,0 +1,10 @@
+#Write a program to accept a number and check whether the number is divisible by 3 as well as 5. Otherwise, decide:
+# (a) Is the number divisible by 3 and not by 5?
+# (b) Is the number divisible by 5 and not by 3?
+# (c) Is the number neither divisible by 3 nor by 5?
+# The program displays the message accordingly.
+
+a = int (input("Enter a number : "))
+if a%3 == 0 and a%5 == 0:
+  print("Divisible by both 3 as well as 5.")
+elif 
