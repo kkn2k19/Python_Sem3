05e16562f687f20d4bcd457f409958116dc0de5f
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-19 00:31:59 +0530

    Create C1.py

---
+++ C1.py
@@ -0,0 +1,13 @@
+# Write a program in python to input the lower and upper limit and print all the prime number lies between the range.
+# Also print the following information 
+# a. count number of prime numbers.
+# b. count number of 2 digit prime number.
+
+a = int (input ("Lower Limit : "))
+b = int (input ("Upper Limit : "))
+
+for i in range (a, b+1):
+  c = 0
+  for j in range (2, i)
+
+  WAIT UPLOADING IT>>>>>>>
