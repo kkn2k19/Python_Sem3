a37b7a20f37c0fe4788b48465a5ceef11d7287ef
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-20 03:08:16 +0530

    Create C3.py

---
+++ C3.py
@@ -0,0 +1,8 @@
+# 3. Program to determine whether a given number is a happy number.
+#    The happy number can be defined as a number which will yield 1 when it is replaced by the sum of the square of its digits repeatedly. 
+#    If this process results in an endless cycle of numbers containing 4, then the number is called an unhappy number.(use math module)
+#    For example, 32 is a happy number as the process yields 1 as follows
+#    32 + 22 = 13
+#    12 + 32 = 10
+#    12 + 02 = 1
+
