f90fd840524735f48511ee6d5852771e7ac2accf
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-20 03:07:04 +0530

    Create C2.py

---
+++ C2.py
@@ -0,0 +1,6 @@
+# 2 . Write a menu driven program in python which will accept a number and choice as ‘a’,’b’,’c’ from the user and
+#     calculate the following according to the user choice.(use user defined function):
+#     a.	Factorial of each digit (Calculate factorial of each digit and return the the same).
+#     b.	Reverse of that number (Calculate Reverse of the number and display) 
+#     c.	Factor’s of that number (Display factors of the number)
+
