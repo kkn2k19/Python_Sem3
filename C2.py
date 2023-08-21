77c40854dccf2996858f1ac629488c0f869a6fd6
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-21 21:19:43 +0530

    Update C2.py

--- C2.py
+++ C2.py
@@ -4,3 +4,37 @@
 #     b.	Reverse of that number (Calculate Reverse of the number and display) 
 #     c.	Factor’s of that number (Display factors of the number)
 
+choice = input("Enter 'a' for Factorial of each digit.\n'b' for Reverse of Number.\n'c' for Displaying Factors of a Number. : ")
+n=int(input("Enter n : "))
+    
+def fact(d):
+    f = 1
+    for i in range (1, d+1):
+        f = f*i
+    print("Factorial of '",d,"' : ", f)
+
+def rev(n):
+    r = 0
+    i = n
+    while i>0:
+        d=i%10
+        r=r*10+d
+        i=i//10
+    print("Reverse of ",n," : ", r)
+
+def factors(n):
+    print("Factors of ", n, " : ", end ="")
+    for i in range(1, n+1):
+        if n%i==0:
+            print(i, end=" ")
+
+if choice == 'a':
+    i=n
+    while i>0:
+        d=i%10
+        fact(d)
+        i=i//10
+elif choice=='b':
+    rev(n)
+elif choice=='c':
+    factors(n)
