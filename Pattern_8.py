75b5b99753c4ddf2e0d475bb25afe0d1a9970aa6
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-08-23 22:26:20 +0530

    Update Pattern_8.py

--- Pattern_8.py
+++ Pattern_8.py
@@ -5,7 +5,8 @@
 #  1 3 5 7 9
 
 for i in range(1, 6):
-  a=2*(6-i)-1for j in range(1, 6):
-    print(a, end=" ")
-    a=a-2
+  a=2*(6-i)-1
+  for j in range(1, i+1):
+    print(a, end="")
+    a=a+2
   print()
