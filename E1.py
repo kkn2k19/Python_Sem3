fd0b326376424b92e45f15f453e1daaf02b3711f
kkn2k19 <83773820+kkn2k19@users.noreply.github.com>
2023-09-02 11:15:12 +0530

    Update E1.py

--- E1.py
+++ E1.py
@@ -2,22 +2,24 @@
 # Find 2nd largest number from the list.
 
 def remove_duplicate(arr):
-  print("List after removing duplicates : ")
-  for i in range(0, n):
-    c=0
-    for j in range(i+1, n):
-      if arr[i]==arr[j]:
-        c=1
-        break
-    if c==0:
-      print(arr[i], end=" ")
+    unique_arr=[]
+    for i in range(0, n):
+        c=0
+        for j in range(i+1, n):
+            if arr[i]==arr[j]:
+                c=1
+                break
+        if c==0:
+            unique_arr.append(arr[i])
+    return unique_arr
 
-def second_largest(arr):
-  for i in range(0, n):
-    for j in range(i+1, n):
-      if arr[i]>arr[j]:
-        arr[i]=arr[j]
-  print("Second Largest Number from List : ", arr[n-2])
+def second_largest(unique_arr):
+    m=len(unique_arr)
+    for i in range(0, m):
+        for j in range(i+1, m):
+            if unique_arr[i]>unique_arr[j]:
+                unique_arr[i]=unique_arr[j]
+    return unique_arr[m-2]
 
 n=int(input("Enter no. of elements : "))
 arr=[]
@@ -25,5 +27,7 @@ for i in range(1, n+1):
   k=int(input("Enter value : "))
   arr.append(k)
 print("original List : ", arr)
-second_largest(arr)
-remove_duplicate(arr)
+unique_arr=remove_duplicate(arr)
+print("List after Removing Duplicates : ", unique_arr)
+print("Second Largest Number from List : ", second_largest(unique_arr))
+
